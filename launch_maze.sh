3b5653a790b1f55b7e10aba7ca236d930994e5bd
DimuthuAro <dimuthuaro@gmail.com>
2025-10-20 19:32:00 +0530

    FinzlizedRos

--- launch_maze.sh
+++ launch_maze.sh
@@ -2,6 +2,12 @@
 
 # Fix for snap library conflicts - remove snap paths from library search
 unset LD_PRELOAD
+
+# Source ROS 2 first to get all ROS libraries
+source /opt/ros/jazzy/setup.bash
+source ~/turtlebot3_ws/install/setup.bash
+
+# Now prepend system libraries to override any snap libraries
 export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
 
 # Remove snap paths if they exist
@@ -9,10 +15,6 @@ if [[ ":$LD_LIBRARY_PATH:" == *":/snap/"* ]]; then
     LD_LIBRARY_PATH=$(echo "$LD_LIBRARY_PATH" | tr ':' '\n' | grep -v '/snap/' | tr '\n' ':' | sed 's/:$//')
     export LD_LIBRARY_PATH
 fi
-
-# Source ROS 2
-source /opt/ros/jazzy/setup.bash
-source ~/turtlebot3_ws/install/setup.bash
 
 # Launch the simulation
 ros2 launch turtlebot3_maze slam.launch.py "$@"
